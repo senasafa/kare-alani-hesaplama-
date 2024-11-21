76b955e3fa68d5a53ff83cc53f226ed36fb7785b
senasafa <senasafa8@gmail.com>
2024-11-22 01:14:27 +0300

    Add files via upload

---
+++ kare alanı hesaplama.cs
@@ -0,0 +1,11 @@
+﻿using System;
+double kenar, alan;
+Console.WriteLine("kenar");
+kenar = Convert.ToDouble(Console.ReadLine());
+
+alan = kenar * kenar;
+Console.WriteLine("alan");
+Console.WriteLine(alan);
+
+Console.ReadLine();
+
